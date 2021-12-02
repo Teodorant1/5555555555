ba1056d35762d461d7021a3e959c115d14d3ae3f
Teodorant1 <52867608+Teodorant1@users.noreply.github.com>
2021-12-02 18:03:56 +0100

    done

--- spring-boot/src/main/java/com/example/springboot/Application.java
+++ spring-boot/src/main/java/com/example/springboot/Application.java
@@ -3,6 +3,7 @@ package com.example.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//finished
 
 
 @SpringBootApplication
